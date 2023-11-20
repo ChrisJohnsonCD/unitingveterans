d864b613e0b90b5acf67434f9cfafd510c51ca8a
ChrisJohnsonCD <129870653+ChrisJohnsonCD@users.noreply.github.com>
2023-11-20 14:34:02 -0700

    main page and menu styled

--- app/sprint1/page.js
+++ app/sprint1/page.js
@@ -1,27 +1,55 @@
 import React from 'react';
 import VeteransPage from './veterans';
+import Menu from './menu'; 
 
 
 export default function Page() {
-    const gradientStyle = {
-        background: 'radial-gradient(circle,  #ffffff, #e63946)',
-        padding: '20px', // Add padding to adjust the size of the circular gradient
-        textAlign: 'center', // Center the text
-        borderBottom: '2px solid gold', // Border style
-      };
+  const headerStyle = {
+    background: 'radial-gradient(circle,#ffffff, #e63946)',
+    padding: '20px',
+    textAlign: 'center',
+    border: '2px solid gold',
+    color: 'black',
+    fontFamily: 'Old English Text MT, serif',
+    fontSize: '2.5rem',
+  };
 
+  const footerStyle = {
+    background: 'radial-gradient(circle,#ffffff, #e63946)',
+    padding: '20px',
+    textAlign: 'center',
+    border: '2px solid gold',
+    color: 'black',
+    fontFamily: 'Old English Text MT, serif',
+    fontSize: '.85rem',
+  };
 
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* Header */}
+      <header style={headerStyle} className="p-8 text-center border-b-2 border-gold">
+        <h1>
+          Uniting Veterans
+        </h1>
+      </header>
 
-    return (
-      <main>
-        <div style= {gradientStyle}>
-        <h1 className="text-2xl font-bold mb-4 text-center">
-            Uniting Veterans</h1>
+       {/* Main Content */}
+      <main className="flex-grow bg-black p-20 flex">
+        <div className="flex-none">
+          <Menu />
         </div>
-        
-      <VeteransPage />
-      
+        <div className="flex-grow">
+          <VeteransPage />
+        </div>
       </main>
-    );
-  }
-        
+    
+      
+       {/* Footer */}
+       <footer style={footerStyle} className="p-8 text-center border-t-2 border-gold">
+        <p>
+          &copy; 2023 Chris Johnson, CD Website. All rights reserved.
+        </p>
+        </footer>
+    </div>
+  );
+}
